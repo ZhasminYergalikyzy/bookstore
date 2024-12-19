f77fc2f928f8ca7b760c51cb4266e5f352f1899e
Zhasmin Yergalikyzy <145698310+ZhasminYergalikyzy@users.noreply.github.com>
2024-12-19 22:54:09 +0500

    main2.go

--- main2.go
+++ main2.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv" // Для преобразования строки в int
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -14,14 +14,14 @@ import (
 
 var db *sql.DB
 
-// Модель пользователя
+
 type User struct {
 	ID    int    `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
 
-// Инициализация соединения с базой данных
+
 func init() {
 	var err error
 	connStr := "user=zhasmin password=062442 dbname=bookstore sslmode=disable"
@@ -36,7 +36,6 @@ func init() {
 	fmt.Println("Connected to the database")
 }
 
-// Создание нового пользователя
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
@@ -45,7 +44,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Вставка нового пользователя в базу данных
+
 	sqlStatement := `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id`
 	err = db.QueryRow(sqlStatement, user.Name, user.Email).Scan(&user.ID)
 	if err != nil {
@@ -57,7 +56,7 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(user)
 }
 
-// Получение всех пользователей
+
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT id, name, email FROM users")
 	if err != nil {
@@ -83,60 +82,47 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(users)
 }
 
-// Редактирование пользователя
+
 func updateUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	// Преобразуем id из строки в int
 	idInt, err := strconv.Atoi(id)
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
-
-	// Декодируем запрос с новыми данными
 	err = json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	// Обновление данных пользователя в базе
 	sqlStatement := `UPDATE users SET name=$1, email=$2 WHERE id=$3`
 	_, err = db.Exec(sqlStatement, user.Name, user.Email, idInt)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Возвращаем обновленные данные
 	user.ID = idInt
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(user)
 }
 
-// Удаление пользователя
+
 func deleteUser(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-
-	// Преобразуем id из строки в int
 	idInt, err := strconv.Atoi(id)
 	if err != nil {
 		http.Error(w, "Invalid ID", http.StatusBadRequest)
 		return
 	}
-
-	// Удаляем пользователя из базы данных
 	sqlStatement := `DELETE FROM users WHERE id=$1`
 	_, err = db.Exec(sqlStatement, idInt)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "User with ID %d was deleted", idInt)
 }
@@ -144,7 +130,6 @@ func deleteUser(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 
-	// Маршруты
 	r.HandleFunc("/create", createUser).Methods("POST")
 	r.HandleFunc("/users", getUsers).Methods("GET")
 	r.HandleFunc("/users/{id}", updateUser).Methods("PUT")
